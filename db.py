28c3e46a872f060a20842f83e0790bfac1d6ab4e
yeowheng <yeowheng1999@gmail.com>
2023-04-22 08:46:18 +0800

    update

--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 import asyncpg
-from asyncpg import UniqueViolationError
 
 class Database:
     def __init__(self, user, pw, host, db):
@@ -9,11 +8,13 @@ class Database:
         self.host = host
         self.db = db
 
+
     async def create_pool(self):
         self.pool = await asyncpg.create_pool(
             host=self.host, database=self.db, user=self.user, password=self.pw
         )
         print("Pool successfully created.")
+
 
     async def create_tables(self) -> None:
         await self.pool.execute(
@@ -24,22 +25,19 @@ class Database:
             master_password TEXT
             )"""
         )
-        print("User Profile Table successfully created.")
 
         await self.pool.execute(
-            """CREATE TABLE IF NOT EXISTS password_storer (
+            """CREATE TABLE IF NOT EXISTS platform_password (
             id SERIAL PRIMARY KEY,
-            user_id BIGINT references discord_user_profile(user_id),
+            user_id BIGINT,
             platform TEXT, 
             password TEXT
             )"""
-        )
-        print("2NF table created")
 
-        
     async def close_pool(self):
         await self.pool.close()
         print("Pool closed")
+
 
     async def check_user_exist(self, user_id):
         res = await self.pool.fetchrow(
@@ -50,11 +48,68 @@ class Database:
             
         return False
 
-    async def data_insert(self, user_id, user_tag, master_password):
+
+    async def user_insert(self, user_id, user_tag, master_password):
         await self.pool.execute(
             """ INSERT INTO discord_user_profile (user_id, user_tag, master_password)
                                 VALUES ($1, $2, $3)
                                 RETURNING user_id""",
         user_id, user_tag, master_password)
+
+    
+    async def check_master_password(self, user_id, master_password):
+        res = await self.pool.fetchrow( 
+            """ SELECT user_id FROM discord_user_profile WHERE user_id = $1 AND master_password = $2 """, user_id, master_password
+        )
+        if res:
+            return True
+        
+        return False
+
+
+    async def retrieve_platform(self, user_id):
+        res = await self.pool.fetch(
+            """ SELECT DISTINCT(platform) FROM platform_password WHERE user_id = $1 """, user_id
+        )
+        if res:
+            return res
+        
+        return False
+
+    
+    async def retrieve_all_password(self, user_id):
+        res = await self.pool.fetch(
+            """ SELECT platform, password FROM platform_password WHERE user_id = $1 """, user_id
+        )
+
+        if res:
+            return res
+        
+        return False
+    
+
+    async def retrieve_platform_password(self, user_id, platform):
+        res = await self.pool.fetch(
+            """ SELECT platform, password FROM platform_password WHERE user_id = $1 AND platform = $2 """, user_id, platform
+        )
+
+        if res:
+            return res
+        
+        return False
+
+    
+    async def store_password(self, user_id, platform, password):
+        res = await self.pool.execute(
+            """ INSERT INTO platform_password (user_id, platform, password)
+                VALUES ($1, $2, $3) """, user_id, platform, password
+        )
+
+        if res:
+            return True
+        
+        return False
+    
+    
 
             
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.errors import MissingRequiredArgument
 from dotenv import load_dotenv
 import os
 from db import Database
@@ -12,21 +13,7 @@ DB_PW = os.environ["POSTGRES_PASSWORD"]
 DB_HOST = os.environ["POSTGRES_HOST"]
 DB = os.environ["POSTGRES_DB"]
 
-class App:
-    def __init__(self, database: Database):
-        self.db = database
-
-    async def create_pool(self):
-        await self.db.create_pool()
-
-    async def create_tables(self):
-        await self.db.create_tables()
-    
-    async def data_insert(self, user_id, user_tag, master_passsword):
-        await self.db.data_insert(user_id, user_tag, master_passsword)
-
 db = Database(DB_USER, DB_PW, DB_HOST, DB)
-database = App(db)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -51,17 +38,200 @@ async def start(ctx):
 
 @bot.command()
 @commands.dm_only()
-async def register_master_password(ctx: commands.Context, *, args):
+async def register_master_password(ctx: commands.Context):
+
     author_id = ctx.author.id
     user_exist = await db.check_user_exist(author_id)
 
     if user_exist:
         await ctx.author.send(f"You have already registered. Do you want to reset? (not implemented yet)")
+        return
+
+    await ctx.author.send("Type in your master password")
+
+    try:
+        msg = await bot.wait_for("message", timeout=30)
+
+        if msg:
+            full_user_tag = ctx.author.name + "#" +  ctx.author.discriminator
+            await db.data_insert(author_id, full_user_tag, msg.content)
+            await ctx.author.send(f"You have chosen {msg.content} as your master password")
+
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you didn't reply in time!")
     
-    else:
-        full_user_tag = ctx.author.name + "#" +  ctx.author.discriminator
-        await database.data_insert(author_id, full_user_tag, args)
-        await ctx.author.send(f"You have chosen {args} as your password")
+
+@bot.command()
+@commands.dm_only()
+async def platform_password_stored(ctx: commands.Context):
+
+    try:
+        await ctx.author.send("Enter your master password")
+        msg = await bot.wait_for("message", timeout=30)
+        if msg:
+            author_id = ctx.author.id
+
+            m_pass = await db.check_master_password(author_id, msg.content)
+            if not m_pass:
+                await ctx.author.send("Your master password do not match")
+                return
+
+            res = await db.retrieve_platform(author_id)
+
+            if not res:
+                await ctx.author.send("You currently have not saved any password with this bot")
+                return
+
+            platform_stored = "All platform password stored with us:"
+            
+            res_val = [dict(x) for x in res]
+            for platforms in res_val:
+                platform_stored += f"\n{platforms['platform']}"
+            
+            await ctx.author.send(platform_stored)
+
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you didn't reply in time!")
+
+
+@bot.command()
+@commands.dm_only()
+async def save_platform_password(ctx: commands.Context):
+
+    try:
+        await ctx.author.send("Enter the platform of your choice")
+        platform_msg = await bot.wait_for("message", timeout=30)
+        platform = platform_msg.content
+
+        await ctx.author.send(f"Enter the password for {platform}")
+        password_msg = await bot.wait_for("message", timeout=30)
+        password = password_msg.content
+
+        if platform and password:
+            author_id = ctx.author.id
+            res = await db.store_password(author_id, platform, password)
+
+            if not res:
+                await ctx.author.send(f"Something went wrong...")
+                return
+
+            await ctx.author.send(f"Successfully saved password for {platform}")
+
+            
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you didn't reply in time!")
+
+
+@bot.command()
+@commands.dm_only()
+async def view_saved_password(ctx: commands.Context):
+    choice_output = "1. View all platform passwords\n2. Choose platform password to retrieve\n3. Quit"
+    author_id = ctx.author.id
+
+    try:
+
+        await ctx.author.send("Enter your master password")
+        m_pw_msg = await bot.wait_for("message", timeout=30)
+        if m_pw_msg:
+
+            m_pass = await db.check_master_password(author_id, m_pw_msg.content)
+            if not m_pass:
+                await ctx.author.send("Your master password do not match")
+                return
+
+        await ctx.author.send(choice_output)
+
+        choice_msg = await bot.wait_for("message", timeout=30)
+        choice = choice_msg.content
+
+        while True:
+            if choice == "3":
+                break
+
+            if choice != "1" and choice != "2":
+                await ctx.author.send("Invalid option selected.")
+                await ctx.author.send(choice_output)
+                choice_msg = await bot.wait_for("message", timeout=30)
+                continue
+
+            break
+            
+        if choice == "1":
+            platform_res = await db.retrieve_platform(author_id)
+
+            if not platform_res:
+                await ctx.author.send("You currently have not saved any password with this bot")
+                return
+
+            res = await db.retrieve_all_password(author_id)
+            if not res:
+                await ctx.author.send("Something went wrong...")
+                return
+
+            passwords = "Passwords - \n"
+            res_val = [dict(x) for x in res]
+            
+            for platforms in res_val:
+                passwords += f"{platforms['platform']}: {platforms['password']}\n"
+            
+            await ctx.author.send(passwords)
+        
+        if choice == "2":
+            platform_arr = []
+            platform_res = await db.retrieve_platform(author_id)
+
+            if not platform_res:
+                await ctx.author.send("You currently have not saved any password with this bot")
+                return
+
+            platform_stored = "Choose the platform: \n"
+            
+            res_val = [dict(x) for x in platform_res]
+            for idx, platform in enumerate(res_val, start=1):
+                platform_arr.append(platform['platform'])
+                platform_stored += f"{idx}: {platform['platform']}\n"
+            
+            await ctx.author.send(platform_stored)
+
+            while True:
+                platform_choice_msg = await bot.wait_for("message", timeout=30)
+                platform_choice = platform_choice_msg.content
+                
+                try:
+                    res = await db.retrieve_platform_password(author_id, platform_arr[int(platform_choice) -1])
+                    
+                    if not res:
+                        await ctx.author.send("Something went wrong...")
+                        return
+
+                    res_val = [dict(x) for x in res]
+
+                    passwords = ""
+                    for platforms in res_val:
+                        passwords += f"{platforms['platform']}: {platforms['password']}\n"
+
+                    await ctx.author.send(passwords)
+                    
+                except IndexError:
+                    await ctx.author.send("Invalid option selected.")
+                    await ctx.author.send(platform_stored)
+                    continue
+
+
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you didn't reply in time!")
+
+    
+
+    
+            
+        
+    
+
+
+
+
+        
 
         
 
